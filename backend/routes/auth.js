593b28d747715d0ede53021428a633e82b676b04
venuchilukala <venuchilukala07@gmail.com>
2024-09-07 17:34:04 +0530

    creating thunder client collection

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,23 +4,39 @@ const User = require('../models/User')
 const router = express.Router();
 const {body, validationResult} = require('express-validator')
 
-router.post('/',[
+//creating user using POST and no login is required
+router.post('/createUser',[
     body('name','Enter Valid Name').isLength({min : 3}),
     body('email','Enter valid Email').isEmail(),
     body('password','Enter valid Password').isLength({min : 5}),
-],(req, res)=>{
+],
+async (req, res)=>{
+    //If there are errors return bad request and errors
     const error = validationResult(req);
     if(!error.isEmpty()){
         return res.status(400).json({error : error.array()})
     }
-    User.create({
+    try{
+    // check whether the user exists already
+    let user = await User.findOne({email : req.body.email})
+    if(user){
+        return res.status(400).json({error : 'Sorry user with this email already exists'});
+    }
+    //create a user
+    user = await User.create({
         name : req.body.name,
         email : req.body.email,
         password : req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err) 
-        res.json({error : 'Please enter a unique value for email', message : err.message})});
-    
+    })
+    // .then(user => res.json(user))
+    // .catch(err => {console.log(err) 
+    // res.json({error : 'Please enter a unique value for email', message : err.message})});
+    res.json({user})
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send('some error occured');
+    }
 })
+
 
 module.exports = router;

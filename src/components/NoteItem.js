d56e5022c6a0224ad09b13d6f5c3ad578440a560
venuchilukala <venuchilukala07@gmail.com>
2024-09-08 23:28:43 +0530

    Added icons and alert component

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -8,6 +8,8 @@ const NoteItem = (props) => {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
+          <i className="fa-regular fa-trash-can mx-3"></i>
+          <i className="fa-regular fa-pen-to-square mx-5"></i>
         </div>
       </div>
     </div>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -10,7 +10,7 @@ const Notes = () => {
     <div className="row">
       <h2>Your Notes</h2>
       {notes.map((note) => {
-        return <NoteItem note={note}/>;
+        return <NoteItem key={note._id} note={note}/>;
       })}
     </div>
   );

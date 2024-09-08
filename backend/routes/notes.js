8354ab558927faaf7bf2ae6be0a29d2b86ff2bd6
venuchilukala <venuchilukala07@gmail.com>
2024-09-08 17:14:14 +0530

    updating the notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -29,17 +29,38 @@ router.post('/addnote', fetchuser,[
         if(!error.isEmpty()){
             return res.status(400).json({error : error.array()})
         }
-    
+        
         const note = new Note({
             title, description, tag, user : req.user.id
         })
         const savedNote = await note.save()
-    
+        
         res.json(savedNote);
     }catch(error){
         console.error(error.message);
         res.status(500).send('Internal Server Error');
     }
+})
+
+//Route : 3 - update the note using PUT: /api/notes/updatenote/:id Login required
+router.put('/updatenote/:id', fetchuser, async (req, res)=>{
+    const {title, description, tag} = req.body;
+    //create a new note object
+    const newNote ={};
+    if(title){newNote.title = title}
+    if(description){newNote.description = description}
+    if(tag){newNote.tag = tag}
+
+    //Find the note to be updated and update it
+    let note = await  Note.findById(req.params.id);
+    if(!note){res.status(401).send("Not Found")}
+    
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set : newNote}, {new : true})
+    res.json({note})   
 })
 
 module.exports = router;

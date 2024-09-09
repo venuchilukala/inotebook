17e0ba96a99d4d31abed58d37b21499aabc33f69
venuchilukala <venuchilukala07@gmail.com>
2024-09-09 19:49:49 +0530

    deleting a node

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
   let location = useLocation();
 
-  useEffect(()=>{
-    console.log(location.pathname);
-  },[location])
+  // useEffect(()=>{
+  //   console.log(location.pathname);
+  // },[location])
 
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+
 
 const NoteItem = (props) => {
+  const context = useContext(noteContext);
+  const {deleteNote} = context;
   const { note } = props;
   return (
     <div className="col-md-3">
@@ -8,7 +12,7 @@ const NoteItem = (props) => {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
-          <i className="fa-regular fa-trash-can mx-3"></i>
+          <i className="fa-regular fa-trash-can mx-3" onClick={()=>{deleteNote(note._id)}}></i>
           <i className="fa-regular fa-pen-to-square mx-5"></i>
         </div>
       </div>
